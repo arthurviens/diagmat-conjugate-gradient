a69d9465be02c5f103b397ea88b5d8f0fc5b22e8
Arthur VIENS <arthur.viens@mines-paristech.fr>
2021-12-14 10:41:34 +0100

    Définition des méthodes de CG dans un autre fichier

---
+++ src/conjGrad.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <mpi.h>
+#include <vector>
+#include "DummyDistributedVector.hpp"
+#include "DistributedDiagonalMatrix.hpp"
+
+extern bool debug;
+
+DummyDistributedVector CG(
+    int rank,
+    const DistributedDiagonalMatrix &A,
+    const DummyDistributedVector &b,
+    double rtol=1e-6, int maxiter=1000);
+
+DummyDistributedVector ImprovedCG(
+  int rank,
+  const DistributedDiagonalMatrix &A,
+  const DummyDistributedVector &b,
+  double rtol=1e-6, int maxiter=1000);
+
+DummyDistributedVector ChronopoulosGearCG(
+    int rank,
+    const DistributedDiagonalMatrix &A,
+    const DummyDistributedVector &b,
+    double rtol=1e-6, int maxiter=1000);
