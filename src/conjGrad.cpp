a9114f4419d8c4e8dcc71f7892fabc23ae9e0295
Abdeljelil MAADI <abdeljelil.maadi@front-in1.cemef>
2022-01-08 23:26:58 +0100

    third update chronopolous CG

--- src/conjGrad.cpp
+++ src/conjGrad.cpp
@@ -232,7 +232,7 @@ DummyDistributedVector Preconditionned_ChronopoulosGearCG(
 
     {
       // Allocation
-      double alpha;
+      double alpha, beta, gamma, delta, prev_gamma;
       double nr, nr0;
       DummyDistributedVector r(b);
       DummyDistributedVector x(b); x.data.setZero();
@@ -245,4 +245,57 @@ DummyDistributedVector Preconditionned_ChronopoulosGearCG(
 
       // Initialization
       int iter=0;
-      r.transposeProduct(nr0, r);
+      r.transposeProduct(nr0, r); //u0=M^(-1) r0
+      A.product(w,r); // w0 = A U0
+      alpha=r.ltransposeProduct(r)/v.ltransposeProduct(r); // alpha0:=(r0,u0)/(w0,u0)
+      beta=0;
+      gamma=r.ltransposeProduct(r); // gamma0 = (r0,u0)
+
+      if(rank==0) {
+        std::cout<<"Start Preconditionned Chronopoulos CG"<<std::endl;
+        std::cout<<"    Initial residual: "<< sqrt(nr0) <<std::endl;
+        std::cout<<"    Iteration,  Absolute residual,  Relative residual"<<std::endl;
+      }
+
+      MPI_Request req;
+      MPI_Status status;
+
+        // CG-Loop
+        std::vector<double> values(2);
+      do {
+
+        values[0] = r.ltransposeProduct(r);
+        values[1] = r.ltransposeProduct(v);
+        Dummy_MPI_Iallreduce(MPI_IN_PLACE, values.data(), 2, MPI_DOUBLE, MPI_SUM, (*A._comm), req);
+        gamma = values[0];
+        delta = values[1];
+
+      w *= beta; w += u;
+      q *= beta; q += v;
+      x.axpy(alpha, w);
+      r.axpy(-alpha, q);
+      u.transposeProduct(nr, r); // ui+1 = M^-1 ri+1
+      A.product(v,r); // ligne 9
+      gamma = r.ltransposeProduct(r); //ligne 10
+      delta = v.ltransposeProduct(r); //ligne 11
+      beta = gamma/prev_gamma; //logne 12
+      prev_gamma = gamma;
+      alpha = gamma /( delta - beta * gamma / alpha);
+
+
+      iter++;
+
+
+
+      } while((sqrt(nr/nr0)>rtol) && (iter<maxiter));
+
+      if(rank==0) {
+        if(sqrt(nr/nr0)<rtol) {
+      std::cout<<"Converged solution"<<std::endl;
+        } else {
+      std::cout<<"Not converged solution"<<std::endl;
+        }
+      }
+
+      return x;
+      }
