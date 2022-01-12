a7f9325eb7e745e47bf66a4477a84b3266e24069
Manon TOURBIER <manon.tourbier@n-in8.cemef>
2022-01-12 09:11:39 +0100

    Updates # Committer: Manon TOURBIER <manon.tourbier@n-in8.cemef>

--- src/conjGrad.cpp
+++ src/conjGrad.cpp
@@ -13,16 +13,17 @@ bool debug = false;
 DummyDistributedVector GhyselsVanrooseCG(
     int rank,
     const DistributedDiagonalMatrix &A,
+    const DistributedDiagonalMatrix &M,
     const DummyDistributedVector &b,
     double rtol, int maxiter
     )
-    
-{
+    {
     // Allocation
     double alpha, gamma, delta, beta, prev_gamma;
     DummyDistributedVector r(b);
     DummyDistributedVector x(b); x.data.setZero();
     DummyDistributedVector q(r); q.data.setZero();
+    DummyDistributedVector n(r); n.data.setZero();
     DummyDistributedVector v(r); v.data.setZero();
     DummyDistributedVector u(r); u.data.setZero();
     DummyDistributedVector z(r); z.data.setZero();
@@ -32,7 +33,9 @@ DummyDistributedVector GhyselsVanrooseCG(
     DummyDistributedVector m(r); m.data.setZero();
 
     // Initialization
+    double nr, nr0;
     int iter=0;
+    nr = nr0;
     M.product(u, r);   
     r.transposeProduct(nr0, r);
     A.product(w,u);
@@ -52,12 +55,12 @@ DummyDistributedVector GhyselsVanrooseCG(
         A.product(n,m);
         if (iter > 0) {
             beta = gamma / prev_gamma;
-            alpha = gamma / (delta - beta * gamma / alpha )
+            alpha = gamma / (delta - beta * gamma / alpha );
             } else {
                 beta = 0;
                 alpha = gamma / delta;
                 }
-        }
+        
         z *= beta; z += n;
         q *= beta; q += m;
         p *= beta; p += u;
@@ -69,44 +72,47 @@ DummyDistributedVector GhyselsVanrooseCG(
         iter++;
 
       } while((sqrt(nr/nr0)>rtol) && (iter<maxiter));
+      
         if(rank==0) {
-            if( sqrt(nr/nr0) < rtol){
-                std::cout<<"Converged solution"<<std::endl;
-                }
-                else {
-                    std::cout<<"Not converged solution"<<std::endl;
-                    }
-        }
+            if( sqrt(nr/nr0) < rtol)
+            {
 
-      return x;
-}
+            std::cout<<"Converged solution"<<std::endl;
+            }
+            else {
+            std::cout<<"Not converged solution"<<std::endl;
+            }
+        }
+        return x;
+  } 
+ 
 
 
 DummyDistributedVector ChronopoulosGearCG(
-    int rank,
-    const DistributedDiagonalMatrix &A,
-    const DummyDistributedVector &b,
-    double rtol, int maxiter)
-{
+  int rank,
+  const DistributedDiagonalMatrix & A,
+    const DummyDistributedVector & b,
+      double rtol, int maxiter) {
+      
   // Allocation
   double alpha, gamma, delta, beta, prev_gamma;
   double nr, nr0;
   DummyDistributedVector r(b);
-  DummyDistributedVector x(b); x.data.setZero();
-  DummyDistributedVector v(r); v.data.setZero();
-  DummyDistributedVector u(r); u.data.setZero();
-  DummyDistributedVector z(r); z.data.setZero();
-  DummyDistributedVector w(r); w.data.setZero();
-  
+  DummyDistributedVector x(b);x.data.setZero();
+  DummyDistributedVector q(b);q.data.setZero();
+  DummyDistributedVector v(r);v.data.setZero();
+  DummyDistributedVector u(r);u.data.setZero();
+  DummyDistributedVector z(r);z.data.setZero();
+  DummyDistributedVector w(r);w.data.setZero();
+
   // Initialization
-  int iter=0;
+  int iter = 0;
   r.transposeProduct(nr0, r);
   nr = nr0;
-
-  if(rank==0) {
-    std::cout<<"Start Chronopoulos CG"<<std::endl;
-    std::cout<<"    Initial residual: "<< sqrt(nr0) <<std::endl;
-    std::cout<<"    Iteration,  Absolute residual,  Relative residual"<<std::endl;
+  if (rank == 0) {
+    std::cout << "Start Chronopoulos CG" << std::endl;
+    std::cout << "    Initial residual: " << sqrt(nr0) << std::endl;
+    std::cout << "    Iteration,  Absolute residual,  Relative residual" << std::endl;
   }
 
   A.product(v, r);
@@ -114,191 +120,186 @@ DummyDistributedVector ChronopoulosGearCG(
   MPI_Status status;
 
   // CG-Loop
-  std::vector<double> values(2);
+  std::vector < double > values(2);
   do {
-    //A.product(q, w);
     values[0] = r.ltransposeProduct(r);
     values[1] = r.ltransposeProduct(v);
-    Dummy_MPI_Iallreduce(MPI_IN_PLACE, values.data(), 2, MPI_DOUBLE, MPI_SUM, (*A._comm), req);
+    Dummy_MPI_Iallreduce(MPI_IN_PLACE, values.data(), 2, MPI_DOUBLE, MPI_SUM, ( * A._comm), req);
     gamma = values[0];
     delta = values[1];
 
     A.product(u, v);
-
-    MPI_Wait(&req, &status);
+    MPI_Wait( & req, & status);
 
     if (iter > 0) {
       beta = gamma / prev_gamma;
-      alpha = gamma / (delta - beta * gamma / alpha );
+      alpha = gamma / (delta - beta * gamma / alpha);
     } else {
       beta = 0;
       alpha = gamma / delta;
     }
 
-    if(debug && rank==0) {
-      std::cout<<"gamma, delta: "<< gamma << ", " << delta << std::endl;
+    if (debug && rank == 0) {
+      std::cout << "gamma, delta: " << gamma << ", " << delta << std::endl;
     }
 
-    z *= beta; z += u;
-    q *= beta; q += v;
-    w *= beta; w += r;
+    z *= beta;
+    z += u;
+    q *= beta;
+    q += v;
+    w *= beta;
+    w += r;
 
     x.axpy(alpha, w);
     r.axpy(-alpha, q);
     v.axpy(-alpha, z);
 
-    if((debug) && (rank==0) && (iter%100 == 0)) {
-      std::cout<< std::setfill(' ') << std::setw(8);
-      std::cout<< iter << "/" << maxiter << "        ";
-      std::cout << std::scientific << sqrt(nr) << "        " << sqrt(nr/nr0) << std::endl;
+    if ((debug) && (rank == 0) && (iter % 100 == 0)) {
+      std::cout << std::setfill(' ') << std::setw(8);
+      std::cout << iter << "/" << maxiter << "        ";
+      std::cout << std::scientific << sqrt(nr) << "        " << sqrt(nr / nr0) << std::endl;
     }
 
     prev_gamma = gamma;
 
     r.transposeProduct(nr, r);
     iter++;
-  } while((sqrt(nr/nr0)>rtol) && (iter<maxiter));
+  } while ((sqrt(nr / nr0) > rtol) && (iter < maxiter));
 
-
-  if(rank==0) {
-    if(sqrt(nr/nr0)<rtol) {
-  std::cout<<"Converged solution"<<std::endl;
+  if (rank == 0) {
+    if (sqrt(nr / nr0) < rtol) {
+      std::cout << "Converged solution" << std::endl;
     } else {
-  std::cout<<"Not converged solution"<<std::endl;
+      std::cout << "Not converged solution" << std::endl;
     }
   }
 
-  return x;
+  return(x);
 }
-
-
-
 
 DummyDistributedVector CG(
-    int rank,
-    const DistributedDiagonalMatrix &A,
-    const DummyDistributedVector &b,
-    double rtol, int maxiter)
-{
-    // Allocation
-    double alpha, gamma, delta;
-    double nr, nr0;
-    DummyDistributedVector r(b);
-    DummyDistributedVector x(b); x.data.setZero();
-    DummyDistributedVector q(r); q.data.setZero();
-
-    // Initialization
-    int iter=0;
-    r.transposeProduct(nr0, r);
-    nr = nr0;
-    DummyDistributedVector w(r);
-    if(rank==0) {
-      std::cout<<"Start CG"<<std::endl;
-      std::cout<<"    Initial residual: "<< sqrt(nr0) <<std::endl;
-      std::cout<<"    Iteration,  Absolute residual,  Relative residual"<<std::endl;
-    }
-
-    // CG-Loop
-    do {
-        A.product(q, w);
-        r.transposeProduct(gamma, r);
-        w.transposeProduct(delta, q);
-        alpha = gamma/delta;
-
-	if(debug && rank==0) {
-	  std::cout<<"gamma, delta: "<< gamma << ", " << delta << std::endl;
-	}
-
-        x.axpy(alpha, w);
-        r.axpy(-alpha, q);
-
-
-        r.transposeProduct(nr, r);
-        w *= (nr/gamma);
-        w +=r;
-
-        iter++;
-	if((rank==0) && (iter%10 == 0)) {
-	  std::cout<< std::setfill(' ') << std::setw(8);
-	  std::cout<< iter << "/" << maxiter << "        ";
-	  std::cout << std::scientific << sqrt(nr) << "        " << sqrt(nr/nr0) << std::endl;
-	}
-
-    } while ((sqrt(nr/nr0)>rtol) && (iter<maxiter));
-
-    if(rank==0) {
-      if(sqrt(nr/nr0)<rtol) {
-	  std::cout<<"Converged solution"<<std::endl;
-      } else {
-	  std::cout<<"Not converged solution"<<std::endl;
-      }
-    }
-
-    return x;
-}
-
-DummyDistributedVector ImprovedCG(
   int rank,
-  const DistributedDiagonalMatrix &A,
-  const DummyDistributedVector &b,
-  double rtol, int maxiter)
-{
+  const DistributedDiagonalMatrix & A,
+    const DummyDistributedVector & b,
+      double rtol, int maxiter) {
+  
   // Allocation
   double alpha, gamma, delta;
   double nr, nr0;
   DummyDistributedVector r(b);
-  DummyDistributedVector x(b); x.data.setZero();
-  DummyDistributedVector q(r); q.data.setZero();
+  DummyDistributedVector x(b);
+  x.data.setZero();
+  DummyDistributedVector q(r);
+  q.data.setZero();
 
   // Initialization
-  int iter=0;
+  int iter = 0;
   r.transposeProduct(nr0, r);
   nr = nr0;
   DummyDistributedVector w(r);
-  if(rank==0) {
-    std::cout<<"Start ImprovedCG"<<std::endl;
-    std::cout<<"    Initial residual: "<< sqrt(nr0) <<std::endl;
-    std::cout<<"    Iteration,  Absolute residual,  Relative residual"<<std::endl;
+  if (rank == 0) {
+    std::cout << "Start CG" << std::endl;
+    std::cout << "    Initial residual: " << sqrt(nr0) << std::endl;
+    std::cout << "    Iteration,  Absolute residual,  Relative residual" << std::endl;
   }
 
   // CG-Loop
   do {
-      A.product(q, w);
-      r.doubleTransposeProduct(w, gamma, delta, r, q);
-      alpha = gamma/delta;
+    A.product(q, w);
+    r.transposeProduct(gamma, r);
+    w.transposeProduct(delta, q);
+    alpha = gamma / delta;
 
-if(debug && rank==0) {
-  std::cout<<"gamma, delta: "<< gamma << ", " << delta << std::endl;
-}
+    if (debug && rank == 0) {
+      std::cout << "gamma, delta: " << gamma << ", " << delta << std::endl;
+    }
 
-      x.axpy(alpha, w);
-      r.axpy(-alpha, q);
+    x.axpy(alpha, w);
+    r.axpy(-alpha, q);
 
+    r.transposeProduct(nr, r);
+    w *= (nr / gamma);
+    w += r;
 
-      r.transposeProduct(nr, r);
-      w *= (nr/gamma);
-      w +=r;
+    iter++;
+    if ((rank == 0) && (iter % 10 == 0)) {
+      std::cout << std::setfill(' ') << std::setw(8);
+      std::cout << iter << "/" << maxiter << "        ";
+      std::cout << std::scientific << sqrt(nr) << "        " << sqrt(nr / nr0) << std::endl;
+    }
 
-      iter++;
-if(debug && (rank==0) && (iter%10 == 0)) {
-  std::cout<< std::setfill(' ') << std::setw(8);
-  std::cout<< iter << "/" << maxiter << "        ";
-  std::cout << std::scientific << sqrt(nr) << "        " << sqrt(nr/nr0) << std::endl;
-}
+  } while ((sqrt(nr / nr0) > rtol) && (iter < maxiter));
 
-  } while ((sqrt(nr/nr0)>rtol) && (iter<maxiter));
-
-  if(rank==0) {
-    if(sqrt(nr/nr0)<rtol) {
-  std::cout<<"Converged solution"<<std::endl;
+  if (rank == 0) {
+    if (sqrt(nr / nr0) < rtol) {
+      std::cout << "Converged solution" << std::endl;
     } else {
-  std::cout<<"Not converged solution"<<std::endl;
+      std::cout << "Not converged solution" << std::endl;
     }
   }
 
-  return x;
+  return(x);
 }
 
+DummyDistributedVector ImprovedCG(
+  int rank,
+  const DistributedDiagonalMatrix & A,
+    const DummyDistributedVector & b,
+      double rtol, int maxiter) {
+  
+  // Allocation
+  double alpha, gamma, delta;
+  double nr, nr0;
+  DummyDistributedVector r(b);
+  DummyDistributedVector x(b);
+  x.data.setZero();
+  DummyDistributedVector q(r);
+  q.data.setZero();
 
+  // Initialization
+  int iter = 0;
+  r.transposeProduct(nr0, r);
+  nr = nr0;
+  DummyDistributedVector w(r);
+  if (rank == 0) {
+    std::cout << "Start ImprovedCG" << std::endl;
+    std::cout << "    Initial residual: " << sqrt(nr0) << std::endl;
+    std::cout << "    Iteration,  Absolute residual,  Relative residual" << std::endl;
+  }
 
+  // CG-Loop
+  do {
+    A.product(q, w);
+    r.doubleTransposeProduct(w, gamma, delta, r, q);
+    alpha = gamma / delta;
 
+    if (debug && rank == 0) {
+      std::cout << "gamma, delta: " << gamma << ", " << delta << std::endl;
+    }
+
+    x.axpy(alpha, w);
+    r.axpy(-alpha, q);
+
+    r.transposeProduct(nr, r);
+    w *= (nr / gamma);
+    w += r;
+
+    iter++;
+    if (debug && (rank == 0) && (iter % 10 == 0)) {
+      std::cout << std::setfill(' ') << std::setw(8);
+      std::cout << iter << "/" << maxiter << "        ";
+      std::cout << std::scientific << sqrt(nr) << "        " << sqrt(nr / nr0) << std::endl;
+    }
+
+  } while ((sqrt(nr / nr0) > rtol) && (iter < maxiter));
+
+  if (rank == 0) {
+    if (sqrt(nr / nr0) < rtol) {
+      std::cout << "Converged solution" << std::endl;
+    } else {
+      std::cout << "Not converged solution" << std::endl;
+    }
+  }
+
+  return(x);
+}
